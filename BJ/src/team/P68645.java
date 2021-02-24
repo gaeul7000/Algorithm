bfdf5ee8d9f7c90d16942b8ef65b733cc432e159
kyounghun.jun <mandrian@naver.com>
2021-02-24 10:02:37 +0900

    솔직히 검색함

---
+++ BJ/src/team/P68645.java
@@ -0,0 +1,69 @@
+package team;
+
+/* N0.3
+ * 2021.2 4ÁÖÂ÷
+ * 
+ */
+
+class Solution{
+	public Solution() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public int[] solution(int n) {
+	        int[] answer;
+	        int[][] triangle = new int[n][n];
+	        
+	        int max = sum(n);
+	        answer = new int[max];
+	        
+	        for(int i=0;i<n;i++) {
+	        	for(int j=0;j<=i;j++) {
+	        		triangle[i][j]=-1;
+	        	}
+	        }
+	        
+	        int i = 0,j = 0;
+	        int k = 1;
+	        
+	        triangle[i][j] = k;
+	        
+	        while(k<max) {
+	        	while(i+1<n&&triangle[i+1][j]<0&&k<max) {
+	        		triangle[++i][j] = ++k;
+	        	}
+	        	while(j+1<n&&triangle[i][j+1]<0&&k<max) {
+	        		triangle[i][++j] = ++k;
+	        	}
+	        	while(i-1>0&&j-1>0&&triangle[i-1][j-1]<0&&k<max) {
+	        		triangle[--i][--j] = ++k;
+	        	}
+	        }
+	        
+	        k=0;
+	        
+	        for(i=0;i<n;i++) {
+	        	for(j=0;j<=i;j++) {
+	        		answer[k++] = triangle[i][j];
+	        	}
+	        }
+	        return answer;
+	    }
+	 
+	 public int sum(int n) {
+		 int max = 0;
+		 
+		 for(int i=1;i<=n;i++) {
+			 max += i;
+		 }
+		 
+		 return max;
+	 }
+}
+
+public class P68645 {
+		public static void main(String[] args) {
+		}
+
+
+}
