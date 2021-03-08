c6b391e3caa952a81ff07725794e5f242fe8111c
가자리 <gaeul7000@naver.com>
2021-03-08 17:45:16 +0900

    숨바꼭질

---
+++ BJ/src/team/BJ1697.java
@@ -0,0 +1,70 @@
+package team;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/* N0.5
+ * 2021.3 2주차
+ * 
+ */
+
+class Node{
+	int position;
+	int count;
+	
+	public Node(int position, int count) {
+		this.position = position;
+		this.count = count;
+	}
+}
+
+
+class Solution3{
+	public int solution(int n, int m) {
+		int answer=0;
+		final int MAX = 100000;
+		int count=0;
+		boolean[] isVisited = new boolean[MAX]; //방문은 true
+		
+		Queue<Node> q = new LinkedList<Node>();
+		
+		q.add(new Node(n,count));
+		
+		while(!q.isEmpty()) {
+			Node node = q.poll();
+			int pos = node.position;
+			int cot = node.count;
+			System.out.println("수ㅟ치"+pos+"동생위치"+m+"뎁스"+cot);
+			if(pos == m) {
+				answer = node.count;
+				break;
+			}
+			
+			if(!isVisited[pos-1]&&pos-1>=0) {
+				isVisited[pos-1] = true;
+				q.add(new Node(pos-1,cot+1));
+			}
+			if(!isVisited[pos+1]&&pos+1<MAX) {
+				isVisited[pos+1] = true;
+				q.add(new Node(pos+1,cot+1));
+			}
+			if(!isVisited[pos*2]&&pos*2<MAX) {
+				isVisited[pos*2] = true;
+				q.add(new Node(pos*2,cot+1));
+			}
+			
+		}
+				
+		
+		return answer;
+	}
+	
+}
+
+public class BJ1697 {
+
+	public static void main(String[] args) {
+		Solution3 s = new Solution3();
+		System.out.println(s.solution(5, 17));
+	}
+}
