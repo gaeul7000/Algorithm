7d6ff2e44366800f2571faf492f92a8731ca3ca3
가자리 <gaeul7000@naver.com>
2021-03-09 14:39:44 +0900

    나 개멍청이

--- BJ/src/team/BJ1697.java
+++ BJ/src/team/BJ1697.java
@@ -1,6 +1,7 @@
 package team;
 
 import java.util.LinkedList;
+import java.util.ArrayList;
 import java.util.Queue;
 
 /* N0.5
@@ -22,7 +23,7 @@ class Node{
 class Solution3{
 	public int solution(int n, int m) {
 		int answer=0;
-		final int MAX = 100000;
+		final int MAX = 100001;
 		int count=0;
 		boolean[] isVisited = new boolean[MAX]; //방문은 true
 		
@@ -39,7 +40,7 @@ class Solution3{
 				answer = node.count;
 				break;
 			}
-			
+			/*
 			if(!isVisited[pos-1]&&pos-1>=0) {
 				isVisited[pos-1] = true;
 				q.add(new Node(pos-1,cot+1));
@@ -52,6 +53,23 @@ class Solution3{
 				isVisited[pos*2] = true;
 				q.add(new Node(pos*2,cot+1));
 			}
+			*/
+			
+			if(pos-1>=0&&!isVisited[pos-1]) {
+				isVisited[pos-1] = true;
+				q.add(new Node(pos-1,cot+1));
+			}
+			if(pos+1<MAX&&pos+1<m&&!isVisited[pos+1]) { //개멍청이인부분 : pos+1과 pos*2는 당연히 MAX값을 넘어갈 가능성이 있고, 
+														//바운드를 넘어간 인덱스를 참조하면 당연히 에러가난다요.
+														//인덱스값을 먼저 구하고 범위를 넘어갈경우 해당 조건을 아예 타지 않게해야지!!!!!!!!!!!!!!!!!
+				isVisited[pos+1] = true;
+				q.add(new Node(pos+1,cot+1));
+			}
+			if(pos*2<MAX&&pos*2<m&&!isVisited[pos*2]) {
+				isVisited[pos*2] = true;
+				q.add(new Node(pos*2,cot+1));
+			}
+			
 			
 		}
 				
