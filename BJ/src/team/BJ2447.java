cc9e05ccf6f4dcd77708a90ad75bd3fb79efdc8f
gaeul <gaeul7000@naver.com>
2021-03-16 17:09:43 +0900

    수치다

--- BJ/src/team/BJ2447.java
+++ BJ/src/team/BJ2447.java
@@ -1,30 +1,50 @@
 package team;
 
-public class BJ2447 {
-	
-	public static void star(String[][] s, int n) {
-			for(int i=n-1; i>=0; i--) {
-				for(int j=n-1; j>=0; j--) {
-					if(j%3==1&&i%3==1) s[i][j] = " ";
-					else if(n/3<=j&&j<n-n/3&&n/3<=i&&i<n-n/3) s[i][j] = " ";
-					else s[i][j] = "*";
-					}
-				}
+import java.io.*;
 
+public class BJ2447 {
+	static String[][] square;
+	
+	public static void star(int x, int y, int n) {
+		int m = n/3;
 		
-		for(int i=0; i<n; i++) {
-			for(int j=0; j<n; j++) {
-				System.out.print(s[i][j]);
+		if(n == 1) {
+			square[x][y] = "*";
+			return;
+		}
+		
+		for(int i = 0; i < 3; i++) {
+			for(int j = 0; j < 3;j++) {
+				if(i == 1 && j == 1) continue;
+				star(m * i + x, m * j + y, m);
 			}
-			System.out.println(" ");
 		}
 	}
 	
-
 	
-	public static void main(String []args) {
-		String[][] answer = new String[27][27];
+	public static void main(String []args) throws Exception {
 		
-		star(answer, 27);
+		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(bufferedReader.readLine());
+		
+		square = new String[n][n];
+		
+		for(int i=0;i<n;i++) {
+			for(int j=0;j<n;j++) {
+				square[i][j]=" ";
+			}
+		}
+		
+		star(0,0,n);
+		
+		for(int i=0;i<n;i++) {
+			for(int j=0;j<n;j++) {
+				System.out.print(square[i][j]);
+			}
+			System.out.println(" ");
+		}
+		
+
 	}
 }
