0a8962bff87721aeec0923ba408dfad98f6bb2c9
전가을 <gaeul.jun@kbfg.com>
2021-05-04 15:07:34 +0900

    .

--- BJ/src/alone/P1845.java
+++ BJ/src/alone/P1845.java
@@ -8,45 +8,17 @@ class Solution {
         
         int choose = nums.length/2;
         
-        boolean[] isVisited = new boolean[nums.length];
-        
-        combi(nums, isVisited, 0, nums.length, choose);
-        
-        return count;
-    }
-    
-    
-    public void combi(int[] arr, boolean[] isVisited, int start, int n, int r){
-        
-        if(r == 0) checkCount(arr, isVisited, n);
-        else {
-            for(int i = start; i < n; i++){
-                isVisited[i] = true;
-                combi(arr, isVisited, i + 1, n, r - 1);
-                isVisited[i] = false;
-            }
-            
-        }
-        
-    }
-    
-    public void checkCount(int arr[], boolean[] isVisited, int n){
         Map<Integer, Integer> map = new HashMap<>();
         
-        for(int i = 0; i < n; i++){
-            if(isVisited[i]){
-                if(map.containsKey(arr[i]) && map.get(arr[i]) >= 1) map.put(arr[i], map.get(arr[i]) + 1);
-                else map.put(arr[i], 1);
-            }
-            if(isVisited[i]){
-                System.out.print(arr[i]+ " ");
-            }
+        for(int x : nums){
+            if(map.containsKey(x)) map.put(x, map.get(x) + 1);
+            else map.put(x, 1);
         }
-        System.out.println("");
         
-        if(map.size() > count) count = map.size();
-        
+        if(map.size() > choose) return choose;
+        else return map.size();
     }
+    
 }
 public class P1845 {
     public static void main(String[] args) throws Exception {
