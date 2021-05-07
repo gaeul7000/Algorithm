e2c28cd054d43491f98a6f7971f219e2592c451f
전가을 <gaeul.jun@kbfg.com>
2021-05-07 11:15:25 +0900

    중복되는 배열의 반복은 % 로 해결

---
+++ BJ/src/alone/P42840.java
@@ -0,0 +1,39 @@
+package alone;
+
+import java.util.ArrayList;
+
+class Solution {
+    public int[] solution(int[] answers) {
+        int[] answer;
+        
+        int[] s1 = {1, 2, 3, 4, 5};
+        int[] s2 = {2, 1, 2, 3, 2, 4, 2, 5};
+        int[] s3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+
+        int s1A = 0;
+        int s2A = 0;
+        int s3A = 0;
+
+        for(int i = 0; i < answers.length; i++){
+            if(s1[i % s1.length] == answers[i]) s1A++;
+            if(s2[i % s2.length] == answers[i]) s2A++;
+            if(s3[i % s3.length] == answers[i]) s3A++;
+        }
+
+        System.out.println("1" + s1A + "2" + s2A + "3" +s3A);
+        int max = Math.max(Math.max(s1A, s2A), s3A);
+        ArrayList<Integer> ans = new ArrayList<>();
+
+        if(max == s1A) ans.add(1);
+        if(max == s2A) ans.add(2);
+        if(max == s3A) ans.add(3);
+
+        answer = new int[ans.size()];
+
+        for(int i = 0; i < ans.size(); i++){
+            answer[i] = ans.get(i);
+        }
+
+        return answer;
+    }
+}
