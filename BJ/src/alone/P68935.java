0c800f4c5026f07dac49d3b596ccbf5fc3df8bb6
전가을 <gaeul.jun@kbfg.com>
2021-05-13 17:03:41 +0900

    예외처리를 잘하자

--- BJ/src/alone/P68935.java
+++ BJ/src/alone/P68935.java
@@ -1,6 +1,4 @@
 package alone;
-
-
 import java.util.ArrayList;
 
 class Solution {
@@ -8,8 +6,9 @@ class Solution {
         int answer = 0;
         ArrayList<Integer> three = new ArrayList<>();
         int number = 0;
-        
-        while(n > 3){
+
+        if(n < 3) return n;
+        while(n >= 3){
             number = n % 3;
             three.add(number);
             n /= 3;
@@ -18,7 +17,6 @@ class Solution {
                 break;
             }
         }
-        System.out.println(three);
 
         for(int i = three.size() - 1; i >= 0; i--){
             if(i == three.size() - 1){
@@ -28,10 +26,18 @@ class Solution {
             answer += Math.pow(3, (three.size()-1-i)) * three.get(i);
 
         }
-        
+
         return answer;
     }
 }
+
+/*
+*
+*1차 이슈 > 테케 안돌아감
+*
+*>> 3이하 값이 들어왔을때 처리를 안한 나는 바보
+*
+*/
 
 public class P68935 {
     
