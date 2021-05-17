d96b4f96759e03f002e851aa9027769cd1e236dd
전가을 <gaeul.jun@kbfg.com>
2021-05-17 15:06:01 +0900

    방향성 수정 예정ㅈ어정정정ㅈ어

---
+++ BJ/src/team/P77485.java
@@ -0,0 +1,75 @@
+package team;
+
+import java.util.ArrayList;
+
+class Solution {
+    static int[][] arr;
+    public int[] solution(int rows, int columns, int[][] queries) {
+        int[] answer = new int[queries.length];
+        
+        arr = new int[rows][columns];
+        
+        for(int i = 0; i < rows; i++){
+            for(int j = 0; j < columns; j++) arr[i][j] = i * columns + (j + 1);
+        }
+        
+        for(int i = 0; i < queries.length; i++) answer[i] = roll(queries[i]);
+        
+        return answer;
+    }
+    
+    public int roll(int[] query){
+        int x1 = query[0];
+        int y1 = query[1];
+        int x2 = query[2];
+        int y2 = query[3];
+        
+        int i = x1 - 1;
+        int j = y1 - 1;
+        
+        int k = 2 * ((x2 - x1) + (y2 - y1));
+
+        ArrayList<Integer> move = new ArrayList<>();
+
+        int min = 10000;
+        int tmp = arr[i][j];
+
+        move.add(tmp);
+
+        while(k > 1){
+            if(i < x2 - 1 && j == y1 - 1){
+                arr[i][j] = arr[++i][j];
+                move.add(arr[i][j]);
+                k--;
+            }
+            
+            if(i == x2 - 1 && j < y2 - 1){
+                arr[i][j] = arr[i][++j];
+                move.add(arr[i][j]);
+                k--;
+            }
+            
+            if(i > x1 - 1 && j == y2 - 1){
+                arr[i][j] = arr[--i][j];
+                move.add(arr[i][j]);
+                k--;
+            }
+            
+            if(i == x1 - 1 && j > y1){
+                arr[i][j] = arr[i][--j];
+                move.add(arr[i][j]);
+                k--;
+            }
+        }
+        
+        arr[x1 - 1][y1] = tmp;
+
+        for(int x: move) if(min > x) min = x;
+
+        return min;
+    }
+}
+
+public class P77485 {
+    
+}
