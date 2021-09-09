c6bded25b9c7f5a6b2ce37580e740c2b5d06c3ba
전가을 <gaeul.jun@kbfg.com>
2021-09-09 17:55:45 +0900

    뀨

---
+++ BJ/src/alone/P12985.java
@@ -0,0 +1,49 @@
+package alone;
+
+import java.util.Stack;
+
+class Fight {
+    double order;
+    int round;
+
+    public Fight(double o, int r) {
+        this.order = o;
+        this.round = r;
+    }
+}
+
+public class P12985 {
+    public int solution(int n, int a, int b)
+    {
+        Stack<Fight> A = new Stack<>();
+        Stack<Fight> B = new Stack<>();
+        int count = 1;
+
+        A.push(new Fight(a, count));
+        B.push(new Fight(b, count));
+
+        while(true) {
+            count++;
+            if(Math.ceil(A.peek().order/2) == Math.ceil(B.peek().order/2)) break;
+            A.push(new Fight(Math.ceil(A.peek().order/2), count));
+            B.push(new Fight(Math.ceil(B.peek().order/2), count));
+        }
+
+
+        return count;
+    }
+    
+    public static void main(String[] args) throws Exception {
+
+        P12985 p = new P12985();
+
+        int n = 8;
+        int a = 4;
+        int b = 2;
+        
+        int answer = p.solution(n, a, b);
+
+        System.out.println(answer);
+    }
+    
+}
