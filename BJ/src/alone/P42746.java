24674f786130c61e9b2e3b2a12f572de76f1b609
gaeul.jun <gaeul7000@naver.com>
2021-09-18 11:39:15 +0900

    compartor 사용

--- BJ/src/alone/P42746.java
+++ BJ/src/alone/P42746.java
@@ -1,39 +1,42 @@
 package alone;
 
+import java.util.Arrays;
+import java.util.Comparator;
+
 public class P42746 {
-    static int max = 0;
     public String solution(int[] numbers) {
+        String answer = "";
 
-        int[] output = new int[numbers.length];
-        boolean[] visited = new boolean[numbers.length];
-
-        perm(numbers, output, visited, 0, numbers.length, numbers.length);
-        return Integer.toString(max);
-    }
-
-    public void perm(int[] arr, int[] output, boolean[] visited, int depth, int n, int r) {
-        if(depth == r) {
-            String tmp = "";
-            for(int x: output) {
-                tmp += Integer.toString(x);
-            }
-            max = Math.max(max, Integer.parseInt(tmp));
+        String[] order = new String[numbers.length];
+        for(int i = 0; i < numbers.length; i++) {
+            order[i] = Integer.toString(numbers[i]);
         }
-
-        for(int i = 0; i < n; i++) {
-            if(!visited[i]){
-                visited[i] = true;
-                output[depth] = arr[i];
-                perm(arr, output, visited, depth + 1, n, r);
-                visited[i] = false;
+        
+        Arrays.sort(order, new Comparator<String>(){
+            public int compare(String o1, String o2) {
+                if(o1.length() == o2.length()) {
+                    return o2.compareTo(o1);
+                } else if(o1.substring(0,1).equals(o2.substring(0,1))) {
+                    return Integer.parseInt(o2+o1) - Integer.parseInt(o1+o2);
+                }
+                return o2.compareTo(o1);
             }
+        });
+
+        for(String x: order) {
+            if("0".equals(x) && answer.length() == 0) continue;
+            else answer += x;
         }
+        if(answer.length() == 0) answer = "0";
+    
+
+        return answer;
     }
 
     public static void main(String[] args){
         P42746 p = new P42746();
 
-        int[] numbers = {3, 30, 34, 5, 9};
+        int[] numbers = {0,0,0,0};
 
         String answer = p.solution(numbers);
 
