7281ad352a42ddec327cb6d3d7fb763d50dfde07
gaeul.jun <gaeul7000@naver.com>
2021-09-23 22:52:34 +0900

    내림차순 정렬은 생각보다 오래걸려요

--- BJ/src/alone/P42885.java
+++ BJ/src/alone/P42885.java
@@ -1,28 +1,29 @@
 package alone;
 
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Arrays;
 
 public class P42885 {
     public int solution(int[] people, int limit) {
         int answer = 0;
+        Arrays.sort(people);
 
         ArrayList<Integer> peo = new ArrayList<>();
 
         for(int x: people) peo.add(x);
-
-        Collections.sort(peo, Collections.reverseOrder());
-
         int weight = 0;
 
         while(!peo.isEmpty()) {
-            weight += peo.get(0);
-            peo.remove(0);
+            weight += peo.get(peo.size() - 1);
+            peo.remove(peo.size() - 1);
 
             while(!peo.isEmpty()) {
-                if(weight + peo.get(peo.size() - 1) <= limit) {
-                    weight += peo.get(peo.size() - 1);
-                    peo.remove(peo.size() - 1);
+
+                if(limit - weight < 40) break;
+                if(weight + peo.get(0) <= limit) {
+                    weight += peo.get(0);
+                    peo.remove(0);
+                    break;
                 } else break;
             }
 
